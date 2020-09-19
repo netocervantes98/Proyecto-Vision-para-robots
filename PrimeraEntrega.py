d2f0f729f3d1d78f19e70d47e03c1848878dc879
Ernesto Cervantes <ernesto.acacej@gmail.com>
2020-09-18 19:23:13 -0500

    Add median

--- PrimeraEntrega.py
+++ PrimeraEntrega.py
@@ -1,3 +1,12 @@
+# Primera entrega parcial
+# Visión para robots
+# Prof. Gabriel González Sahagún
+
+# A01196642 Ernesto Cervantes Juárez
+# A01196642 Ernesto Cervantes Juárez
+# A01196642 Ernesto Cervantes Juárez
+# A01196642 Ernesto Cervantes Juárez
+
 import numpy as np
 import cv2 as cv
 
@@ -6,19 +15,19 @@ coords = []
 def click(event, x, y, flags, param):
     if event == cv.EVENT_LBUTTONDOWN:
         coords.append((x,y))
-
         if len(coords) == 2:
             submatrix = img[coords[0][0] : coords[1][0], coords[0][1] : coords[1][1]]
             print("min:", np.amin(submatrix))
             print("max:", np.amax(submatrix))
+            print("median:", '{:.3f}'.format(np.median(submatrix)))
             print("average:", '{:.3f}'.format(np.average(submatrix)))
             print("std dev:", '{:.3f}'.format(np.std(submatrix)))
             cv.destroyAllWindows()
 
 # Punto 6
 img = cv.imread('gundam.jpg')
-img = cv.resize(img, (540, 540))
-img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img2 = cv.resize(img, (540, 540))
+img = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 cv.imshow('original', img)
 cv.setMouseCallback('original', click)
 cv.waitKey(0)
